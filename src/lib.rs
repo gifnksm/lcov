e0bfd56dbac9cff2fb9afbe1e82e0a764fba1c6e
gifnksm <makoto.nksm+github@gmail.com>
2018-01-28 00:06:55 +0900

    Refactor line filter

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #[macro_use]
 extern crate failure;
 
+pub use line_filter::Filter as LineFilter;
 pub use reader::{Error as ReadError, Reader};
 pub use record::{ParseRecordError, Record, RecordKind};
 pub use report::{MergeError, Report};
 
-pub mod line_filter;
+mod line_filter;
 mod report;
 mod record;
 mod reader;
--- src/line_filter.rs
+++ src/line_filter.rs
@@ -17,11 +17,11 @@ impl Filter {
     pub fn insert<P, I>(&mut self, path: P, it: I)
     where
         P: Into<PathBuf>,
-        I: IntoIterator<Item = LineHunk>,
+        I: IntoIterator<Item = (u32, u32)>,
     {
         let file = self.files.entry(path.into()).or_insert_with(File::default);
-        for hunk in it {
-            file.add_hunk(hunk);
+        for range in it {
+            file.add_range(range);
         }
         file.normalize();
     }
@@ -47,22 +47,21 @@ struct File {
 }
 
 impl File {
-    fn add_hunk(&mut self, hunk: LineHunk) {
-        let end = self.start2end.entry(hunk.start).or_insert(hunk.end);
-        if *end < hunk.end {
-            *end = hunk.end;
+    fn add_range(&mut self, (start, end): (u32, u32)) {
+        if end < start {
+            return;
         }
-        let start = self.end2start.entry(hunk.end).or_insert(hunk.start);
-        if *start > hunk.start {
-            *start = hunk.start;
-        }
+        let hend = self.start2end.entry(start).or_insert(end);
+        *hend = u32::max(*hend, end);
+        let hstart = self.end2start.entry(end).or_insert(start);
+        *hstart = u32::min(*hstart, start);
     }
 
     fn normalize(&mut self) {
         fn exec<F, I>(map: &mut BTreeMap<u32, u32>, conv_from: F, mut conv_into: I)
         where
-            F: FnMut((u32, u32)) -> LineHunk,
-            I: FnMut(LineHunk) -> (u32, u32),
+            F: FnMut((u32, u32)) -> Hunk,
+            I: FnMut(Hunk) -> (u32, u32),
         {
             let mut iter = mem::replace(map, BTreeMap::new())
                 .into_iter()
@@ -84,21 +83,21 @@ impl File {
 
         exec(
             &mut self.start2end,
-            |(start, end)| LineHunk::new(start, end),
+            |(start, end)| Hunk::new(start, end),
             |hunk| (hunk.start, hunk.end),
         );
         exec(
             &mut self.end2start,
-            |(end, start)| LineHunk::new(start, end),
+            |(end, start)| Hunk::new(start, end),
             |hunk| (hunk.end, hunk.start),
         )
     }
 
 
-    fn contains_range(&self, start: u32, end: u32) -> bool {
-        if let Some((&cend, &cstart)) = self.end2start.range(start..).next() {
-            debug_assert!(cend >= start);
-            if cstart <= end {
+    fn contains_range(&self, (start, end): (u32, u32)) -> bool {
+        if let Some((&hend, &hstart)) = self.end2start.range(start..).next() {
+            debug_assert!(hend >= start);
+            if hstart <= end {
                 return true;
             }
         }
@@ -109,16 +108,16 @@ impl File {
     }
 
     fn contains_line(&self, line: u32) -> bool {
-        if let Some((&cend, &cstart)) = self.end2start.range(line..).next() {
-            debug_assert!(cend >= line);
-            return cstart <= line;
+        if let Some((&hend, &hstart)) = self.end2start.range(line..).next() {
+            debug_assert!(hend >= line);
+            return hstart <= line;
         }
         false
     }
 
     fn execute(&self, section: &mut Section) {
         section.func_list().filter_map(|(key, data)| {
-            if self.contains_range(data.start_line, data.end_line) {
+            if self.contains_range((data.start_line, data.end_line)) {
                 Some((key, data))
             } else {
                 None
@@ -142,17 +141,15 @@ impl File {
 }
 
 #[derive(Debug, Copy, Clone, Default, Eq, PartialEq)]
-pub struct LineHunk {
+struct Hunk {
     start: u32,
     end: u32,
 }
 
-impl LineHunk {
-    pub fn new(start: u32, end: u32) -> Self {
-        if end < start {
-            panic!()
-        }
-        LineHunk { start, end }
+impl Hunk {
+    fn new(start: u32, end: u32) -> Self {
+        assert!(start <= end);
+        Hunk { start, end }
     }
 
     fn join(self, other: Self) -> Option<Self> {
@@ -169,14 +166,14 @@ impl LineHunk {
 
 #[cfg(test)]
 mod tests {
-    use super::LineHunk;
+    use super::Hunk;
 
     #[test]
     fn join() {
         fn check(expect: Option<(u32, u32)>, hunk1: (u32, u32), hunk2: (u32, u32)) {
-            let hunk1 = LineHunk::new(hunk1.0, hunk1.1);
-            let hunk2 = LineHunk::new(hunk2.0, hunk2.1);
-            let expect = expect.map(|expect| LineHunk::new(expect.0, expect.1));
+            let hunk1 = Hunk::new(hunk1.0, hunk1.1);
+            let hunk2 = Hunk::new(hunk2.0, hunk2.1);
+            let expect = expect.map(|expect| Hunk::new(expect.0, expect.1));
             assert_eq!(expect, hunk1.join(hunk2));
             assert_eq!(expect, hunk2.join(hunk1));
             assert_eq!(Some(hunk1), hunk1.join(hunk1));
