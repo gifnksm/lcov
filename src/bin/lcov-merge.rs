86fd1aef816c2f9662cb0871cdb6db54b6196c80
gifnksm <makoto.nksm+github@gmail.com>
2018-02-08 23:11:39 +0900

    Add lcov-merge command

--- src/bin/lcov-merge.rs
+++ src/bin/lcov-merge.rs
@@ -1,5 +1,48 @@
 //! Utility commands to operate and analyze LCOV trace file at blazingly fast.
 
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+#![warn(unused_results)]
+
+extern crate failure;
+extern crate lcov;
+
+use failure::Error;
+use lcov::{Reader, Report};
+use std::{env, process};
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+fn run<I, P>(it: I) -> Result<(), Error>
+where
+    I: IntoIterator<Item = P>,
+    P: AsRef<Path>,
+{
+    let mut report = Report::new();
+
+    for path in it {
+        let file = File::open(path.as_ref())?;
+        let reader = Reader::new(BufReader::new(file));
+        report.merge(reader)?;
+    }
+
+    for rec in report {
+        println!("{}", rec);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run(env::args()) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
 }
